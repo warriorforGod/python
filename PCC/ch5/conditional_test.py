e3fe2d54b5d7f5d8a3f8d48d1d24c8ab18af06b6
Steve Madel <sjmadel@gmail.com>
2018-03-24 12:50:44 -0500

    Added Python Crash Course Folder.

---
+++ PCC/ch5/conditional_test.py
@@ -0,0 +1,100 @@
+fruit = 'apple'
+
+print("Is fruit == 'apple'? I predict True.")
+print(fruit == 'apple')
+
+print("\nIs fruit =='orange'? I predict False.")
+print(fruit == 'orange')
+
+fruit = 'lemon'
+
+print("Is fruit == 'lemon'? I predict True.")
+print(fruit == 'lemon')
+
+print("\nIs fruit == 'apple'? I predict False.")
+print(fruit == 'apple')
+
+car = 'chevy'
+
+print("\nIs car == 'ford'? I predict False.")
+print(car == 'ford')
+
+print("\nIs car == 'chevy'? I predict True.")
+print(car == 'chevy')
+
+number = 23
+
+print("\nIs number == 23? I predict True.")
+print(number == 23)
+
+print("\nIs number == 42? I predict False.")
+print(number == 42)
+
+bird = 'robin'
+
+print("\nIs bird == 'robin'? I predict True.")
+print(bird == 'robin')
+
+print("\nIs bird == 'sparrow'? I predict False.")
+print(bird == 'sparrow')
+
+fish = 'tuna'
+
+print("\nIs fish == 'tuna'? I predict True.")
+print(fish == 'tuna')
+
+print("\nIs fish == 'carp'? I predict False.")
+print(fish == 'carp')
+
+num_trees = 512
+
+print("\nIs num_trees != 512? I predict False.")
+print(num_trees !=512)
+
+print("\nIs num_trees != 256? I predict True.")
+print(num_trees !=256)
+
+name = 'Alan'
+
+print("\nIs name.lower('Alan') == 'alan'? I predict True.")
+print(name.lower() == 'alan')
+
+print("\nIs name.lower('Bill') == 'alan'? I predict False.")
+print(name.lower() == 'bill')
+
+number = 63
+
+print("\nIs number == 63? I predict True.")
+print(number == 63)
+
+print("\nIs number != 63? I predict False.")
+print(number != 63)
+
+print("\nIs number > 5? I predict True.")
+print(number > 5)
+
+print("\nIs number < 5? I predict False.")
+print(number < 5)
+
+print("\nIs number >= 27? I predict True.")
+print(number >= 27)
+
+print("\nIs number <= 27? I predict False.")
+print(number <= 27)
+
+ip_address = '1.1.1.1'
+netmask = 24
+
+print("\nIs ip_address == 1.1.1.1 and netmask == 24? I predict True.")
+print(ip_address == '1.1.1.1' and netmask == 24)
+
+print("\nIs ip_address == 1.1.1.1 or netmask == 32? I predict True.")
+print(ip_address == '1.1.1.1' or netmask == 32)
+
+list = ['Cap', 'Bucky', 'Tony', 'Rhodey']
+
+print("\nIs 'Cap' in list? I predict True.")
+print('Cap' in list)
+
+print("\nIs 'Thor' in list? I predict False.")
+print('Thor' in list)
