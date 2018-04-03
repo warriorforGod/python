81b5b9019b3fe3a088a5fc30dad9d2508a3e1819
Steve Madel <sjmadel@gmail.com>
2018-04-03 17:55:05 -0500

    More twitbot goodness.

---
+++ TwitterBot/local_config.py
@@ -0,0 +1,5 @@
+cons_tok = 'OV3XirRBZeAR6SUjIOa2kufec'
+cons_sec = 'FPyjXX111PRvq2idEWISYcywQDeOKO0kr733gScZHt8N5UKIRV'
+
+app_tok = '1091914164-LXCuLB6R9eOSwSlEpJNkcUB1HtWpEjQJCYrDrms'
+app_sec = 'BdTQSP33G7dn6eCyy9tBlnGKxYmHZsjJACCImWtVNz3xM'
