2f6fbf8f207fbae2d885fbe9137782725c89d956
Steve Madel <sjmadel@gmail.com>
2018-08-14 14:23:25 -0500

    Learning about lists

--- ATBWP/collatz.py
+++ ATBWP/collatz.py
@@ -1,2 +1,17 @@
-def collatz(number)
-    
+def collatz(number):
+  if number % 2 == 0:
+	#print(str(number) + ' // 2')
+   	numEven = number // 2
+	return int(numEven)
+  if number % 2 == 1:
+	#print('3 * ' + str(number) + ' + 1')
+	numOdd = 3 * number + 1   
+	return int(numOdd)
+
+while True:
+	print('Enter number: ')
+	parameter=int(input())
+	if parameter == 1:
+		break
+	else:
+		print parameter
